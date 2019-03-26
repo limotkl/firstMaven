0a0e6664af8225a35884c7933c32b7623646e94f
Fang <limotkl@gmail.com>
2019-03-25 23:30:14 -0700

    add another package

---
+++ src/main/java/firstMaven2/Package2.java
@@ -0,0 +1,7 @@
+package firstMaven2;
+
+public class Package2 {
+	public int cheng(int a,int b){
+		return a*b;
+	}
+}
--- src/test/java/firstMaven/CalculaterTest.java
+++ src/test/java/firstMaven/CalculaterTest.java
@@ -4,16 +4,20 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import firstMaven2.Package2;
+
 public class CalculaterTest {
 	
 	@Test
 	public void testSum() {
 		//fail("Not yet implemented");
 		Calculater testObject = new Calculater();
+		Package2 testObject2 = new Package2();
 		int exRe = 11;
 		int re = testObject.sum(5,6);
 		assertEquals(exRe,re);
 		assertEquals(15,testObject.sum(5,10));
+		assertEquals(50,testObject2.cheng(5,10));
 	}
 	@Test
 	public void testDiv() {
